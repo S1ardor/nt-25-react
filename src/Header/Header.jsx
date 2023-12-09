02c275bb3efa8c0729bafdb257a7e5120207d2a3
S1ardor <quvondiqovs06@gmail.com>
2023-12-09 15:36:12 +1000

    Lesson-10

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,34 +1,9 @@
-import { useState } from 'react';
 import './Header.css';
 
 export const Header = () => {
-
-    const [click, setClick] = useState(false);
-    const btns = () => {
-        setClick(!click);
-        console.log('hello');
-    }
-
-
   return (
-    <div>
-        <div className="container">
-            <div className="box">
-            <h1 className='h1'>Lesson-1</h1>
-               <ul className='text'>
-                  <li><a href="">Hoome</a></li>
-                  <li><a href="">About</a></li>
-                  <li><a href="">Profil</a></li>
-                  <button className='login_btn'>Login</button>
-
-                   <button className='btn'>+</button>
-               </ul>
-            <button onClick={btns} className='button'>+</button>
-            </div>
-
-
-            <button>clicked</button>
-        </div>
-    </div>
+    <>
+    <h1>lesson-10</h1>
+    </>
   )
 }
